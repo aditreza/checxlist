bc9a6e169278e1651afa2652092e460b82185ecf
aditReza <lete.note3@gmail.com>
2017-08-20 05:57:11 +0700

    adding function in js

--- assets/js/tdl.js
+++ assets/js/tdl.js
@@ -9,18 +9,44 @@ function newElement() {
         alert("Kamu harus Menuliskan Sesuatu!");
     } else {
         //  document.getElementById("myUL").appendChild(li); // list dibawah
-        var list = document.getElementById("myUL"); //
+        var list = document.getElementById("checkList"); //
         list.insertBefore(li, list.childNodes[0]); // list di atas
     }
     //document.getElementById("myInput").value = "";
 
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
 
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+
+        }
+
+    }
 }
 
 // list coret dan checklist
 var listCoret = document.querySelector('ul');
+//console.log(listCoret);
 listCoret.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
 }, false);
+
+
+// klik close button
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    //console.log(i);
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
